2f5042a3fe1437b715c41e2a0aa56cbae2044909
Adam Wysocki <adam@adamwysocki.me>
2016-03-02 16:18:40 -0500

    test coverage

---
+++ tests/blanket-options.js
@@ -0,0 +1,33 @@
+/* globals blanket, module */
+
+var options = {
+  modulePrefix: 'mad-bricks',
+  filter: '//.*mad-bricks/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    reporters: ['lcov'],
+    autostart: true
+  },
+  lcovOptions: {
+    outputFile: 'lcov.dat',
+
+    // automatically skip missing files, relative to project's root dir
+    excludeMissingFiles: true, // default false
+
+    // provide a function to rename es6 modules to a file path
+    renamer: function(moduleName){
+      // return a falsy value to skip given module
+      if (moduleName === 'unwanted') { return; }
+
+      var expression = /^APP_NAME/;
+      return moduleName.replace(expression, 'app') + '.js';
+    }
+  },
+};
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
